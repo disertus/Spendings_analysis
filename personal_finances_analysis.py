bf9165dc4cfc945f2e0ba12d83f9399b03e06151
Roman Zahorskyi <zagorsky.roman@gmail.com>
2020-10-18 01:15:26 +0300

    Reformatted the code, added the Visualizer class, sporadically introduced type hints.

--- personal_finances_analysis.py
+++ personal_finances_analysis.py
@@ -1,47 +1,62 @@
 import config as cfg
 import pandas as pd
+import plotly.graph_objects as go
 import requests
+import time
 
 from collections import defaultdict
 from datetime import datetime, timedelta
 from functools import lru_cache
 
 
-# todo: cache the function retrieving data from monobank's endpoint
-# todo: build a class for user differentiation
 # todo: connect a database for retrieved data collection
 # todo: build visualisations with plotly package
 # todo: add telebot notifications
 # todo: transform into a web-app with Flask/Dash, deploy
 
 
-class User:
+class UserData:
 
-    def __init__(self, auth_token):
+    def __init__(self, auth_token: str, account: str):
         self.token = auth_token
+        self.account = account
+        self.spending_source = 'description'
+        self.spending_amount = 'amount'
+        self.spending_time = 'time'
+        self.balance = 'balance'
+        self.cashback_amount = 'cashbackAmount'
 
-    @lru_cache(maxsize=128)  # cache the request result for repetitive usage
-    def retrieve_user_bank_data(self):
+    def form_get_request(self):
         from_date = round((datetime.today() - timedelta(days=30)).timestamp())
-        response_currencies = requests.get('https://api.monobank.ua/bank/currency')
-        response_personal = requests.get('https://api.monobank.ua/personal/client-info',
-                                         headers={'X-Token': self.token})
-        response_statement = requests.get(f'https://api.monobank.ua/personal/statement/{cfg.account}/{from_date}',
+        response_statement = requests.get(f'https://api.monobank.ua/personal/statement/{self.account}/{from_date}',
                                           headers={'X-Token': self.token})
-        return response_statement.json(), response_personal, response_currencies
+        return response_statement
 
-    def json_to_dataframe(self):
+    @lru_cache(maxsize=64)  # cache the request result for repetitive usage
+    def send_get_request(self):
+        try:
+            request_result = self.form_get_request()
+        except:
+            print("Failed to establish connection with bank's endpoint. \nRetrying...")
+            time.sleep(5)
+            self.send_get_request()
+        return request_result
+
+    def parsed_json_to_dict(self) -> dict:
+        raw_data = self.send_get_request().json()
+        df_dict = defaultdict(list)
+        for item in raw_data:
+            df_dict['source'].append(item[self.spending_source])
+            df_dict[self.spending_amount].append(item[self.spending_amount])
+            df_dict[self.spending_time].append(datetime.utcfromtimestamp(item[self.spending_time]))
+            df_dict[self.balance].append(item[self.balance])
+            df_dict[self.cashback_amount].append(item[self.cashback_amount])
+        return df_dict
+
+    def dict_to_dataframe(self):
         """Transfers the data from json into a Dataframe"""
 
-        dataset = self.retrieve_user_bank_data()[0]
-        df_dict = defaultdict(list)
-        for item in dataset:
-            df_dict['source'].append(item['description'])
-            df_dict['amount'].append(item['amount'])
-            df_dict['time'].append(datetime.utcfromtimestamp(item['time']))
-            df_dict['balance'].append(item['balance'])
-            df_dict['cashbackAmount'].append(item['cashbackAmount'])
-        return pd.DataFrame.from_dict(df_dict)
+        return pd.DataFrame.from_dict(self.parsed_json_to_dict())
 
 
 class Analyzer:
@@ -74,15 +89,29 @@ class Analyzer:
         pass
 
 
-user1 = User(cfg.token)
-analysis = Analyzer(user1.json_to_dataframe())
+class Visualizer:
 
-print(user1.json_to_dataframe())
-print(analysis.sum_by_source())
-print(f'\n{analysis.spending_vs_balance_by_date()}')
-print(f'\n{analysis.sum_by_hour()}')
+    def __init__(self, dataset):
+        self.data = dataset
 
-# currencies_dict = {'978': 'Euro',
-#                    '643': 'Ruble',
-#                    '840': 'Dollar',
-#                    '985': 'Zloty'}
+    def show_bar_chart(self, chart_name=None):
+        fig = go.Figure([go.Bar(name=chart_name,
+                                x=self.data.index,
+                                y=self.data)],
+                        layout={"hovermode": "x"})
+        return fig.show()
+
+
+if __name__ == '__main__':
+
+    user1 = UserData(cfg.token, cfg.account)
+    analysis = Analyzer(user1.dict_to_dataframe())
+    viz = Visualizer(analysis.spending_vs_balance_by_date())
+
+    viz.show_bar_chart('Spendings')
+
+    print(user1.dict_to_dataframe())
+    print(analysis.sum_by_source())
+    print(f'\n{analysis.spending_vs_balance_by_date()}')
+    print(f'\n{analysis.sum_by_hour()}')
+
