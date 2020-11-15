2dd7cea588f193c071fa23c99eb5fbb47c606dfe
Roman Zahorskyi <zagorsky.roman@gmail.com>
2020-11-15 23:08:58 +0200

    Added the 'joint' dataframe, possibility to visualize family spendings, updated the format of the visualizations

--- personal_finances_analysis.py
+++ personal_finances_analysis.py
@@ -5,13 +5,13 @@ import requests
 import time
 
 from collections import defaultdict
+from dataclasses import dataclass
 from datetime import datetime, timedelta
 from functools import lru_cache
 
 
-# todo: add the possibility to join data of several users into a single dataset for analysis purposes
-# todo: add user identifiers for spending events
-# todo: add the possibility to create stacked charts with up to 6? distinct users' data
+# todo: replace current visualization method with add_trace
+# todo: add percentage view for the daily spending visualization
 # todo: connect a database for retrieved data collection
 # todo: build visualisations with plotly package
 # todo: add telebot notifications
@@ -20,13 +20,14 @@ from functools import lru_cache
 
 class UserData:
 
-    def __init__(self, auth_token: str, account: str):
-        self.token = auth_token
+    def __init__(self, auth_token: str, account: str, username: str):
         self.account = account
+        self.balance = 'balance'
+        self.name = username
         self.spending_source = 'description'
         self.spending_amount = 'amount'
         self.spending_time = 'time'
-        self.balance = 'balance'
+        self.token = auth_token
 
     @lru_cache(maxsize=64)  # cache the request result for repetitive usage
     def form_get_request(self):
@@ -49,7 +50,7 @@ class UserData:
         df_dict = defaultdict(list)
         for item in raw_data:
             df_dict['source'].append(item[self.spending_source])
-            df_dict[self.spending_amount].append(item[self.spending_amount])
+            df_dict[self.spending_amount].append(int(item[self.spending_amount]) / 100)
             df_dict[self.spending_time].append(datetime.utcfromtimestamp(item[self.spending_time]))
             df_dict[self.balance].append(item[self.balance])
         return df_dict
@@ -57,7 +58,18 @@ class UserData:
     def dict_to_dataframe(self):
         """Transfers the data from json into a Dataframe"""
 
-        return pd.DataFrame.from_dict(self.parsed_json_to_dict())
+        dataframe = pd.DataFrame.from_dict(self.parsed_json_to_dict())
+        dataframe['name'] = self.name
+        return dataframe
+
+
+class FamilyBudget:
+
+    def __init__(self, *df):
+        self.dataframes = df
+
+    def concat_dataframes(self):
+        return pd.concat(self.dataframes)
 
 
 class Analyzer:
@@ -68,26 +80,20 @@ class Analyzer:
     def sum_by_source(self):
         """Calculates the sum by spending source"""
 
-        amount_sum_by_source = self.dataset.groupby(['source']).sum()
-        return amount_sum_by_source['amount'].sort_values(ascending=False) / 100
+        amount_sum_by_source = self.dataset.groupby(['source', 'name']).sum()
+        return amount_sum_by_source.sort_values(by=['amount'], ascending=False).reset_index()
 
     def spending_vs_balance_by_date(self):
         """Spending distribution by date"""
-
-        amount_sum_by_date = self.dataset.groupby(self.dataset['time'].dt.round(freq='D')).sum()
-        return amount_sum_by_date['amount'] / 100
+        amount_sum_by_date = self.dataset
+        amount_sum_by_date['time'] = self.dataset['time'].dt.round(freq='D')
+        return amount_sum_by_date.reset_index()
 
     def sum_by_hour(self):
         """Spending distribution by hour of day"""
 
         amount_sum_by_hour = self.dataset.groupby(self.dataset['time'].dt.hour).sum()
-        return amount_sum_by_hour['amount'] / 100
-
-    def sum_by_user_and_date(self):
-        """Spending by user and date of transaction (for a stacked bar chart)"""
-
-        self.dataset.update(self.dataset['time'].dt.round(freq='D'))
-        pass
+        return amount_sum_by_hour
 
 
 class Visualizer:
@@ -95,30 +101,48 @@ class Visualizer:
     def __init__(self, dataset):
         self.data = dataset
 
-    def show_bar_chart(self, chart_name=None):
+    def show_bar_chart(self, x_coord, y_coord, grouping=None, chart_name=None):
         fig = go.Figure([go.Bar(name=chart_name,
-                                x=self.data.index,
-                                y=self.data)],
-                        layout={"hovermode": "x"})
+                                x=self.data[x_coord],
+                                y=self.data[y_coord])])
+        fig.update_layout(barmode='relative')
+        return fig.show()
+
+    def show_family_budget(self, x_coord, y_coord):
+
+        #use the add_trace method through a loop for a list of dataframes (delete this function)
+        fig = go.Figure([go.Bar(name=user1.name,
+                                x=self.data.query('name == @user1.name')[x_coord],
+                                y=self.data.query('name == @user1.name')[y_coord]),
+                         go.Bar(name=user2.name,
+                                x=self.data.query('name == @user2.name')[x_coord],
+                                y=self.data.query('name == @user2.name')[y_coord])])
+        #fig.update_layout(barmode='relative')
         return fig.show()
 
 
 if __name__ == '__main__':
-
-    user1 = UserData(cfg.token1, cfg.account1)
-    user2 = UserData(cfg.token2, cfg.account2)
+    user1 = UserData(cfg.token1, cfg.account1, 'roman')
+    user2 = UserData(cfg.token2, cfg.account2, 'nika')
     analysis1 = Analyzer(user1.dict_to_dataframe())
     analysis2 = Analyzer(user2.dict_to_dataframe())
     viz1 = Visualizer(analysis1.spending_vs_balance_by_date())
     viz2 = Visualizer(analysis2.spending_vs_balance_by_date())
 
-    #viz1.show_bar_chart('Spendings1')
-    #viz2.show_bar_chart('Spendings2')
+    viz1.show_bar_chart('time', 'amount')
+    viz2.show_bar_chart('time', 'amount')
 
-    print(user1.dict_to_dataframe())
-    print(analysis1.sum_by_source())
     print(f'\n{analysis1.spending_vs_balance_by_date()}')
-    print(f'\n{analysis1.sum_by_hour()}')
+    print(f'by hour \n{analysis1.sum_by_hour()}')
 
-    user1.dict_to_dataframe().to_csv('user1_10_21.csv', index=False)
-    user2.dict_to_dataframe().to_csv('user2_10_21.csv', index=False)
+    family_spendings = FamilyBudget(user1.dict_to_dataframe(), user2.dict_to_dataframe()).concat_dataframes()
+    print(family_spendings.head(50))
+    print(family_spendings.tail(50))
+    print(Analyzer(family_spendings).sum_by_source())
+    print(Analyzer(family_spendings).spending_vs_balance_by_date())
+
+    Visualizer(Analyzer(family_spendings).spending_vs_balance_by_date()).show_family_budget('time', 'amount')
+
+    # temporary gipsy solution for data saving instead of a database
+    # user1.dict_to_dataframe().to_csv('user1_11_15.csv', index=False)
+    # user2.dict_to_dataframe().to_csv('user2_11_15.csv', index=False)
